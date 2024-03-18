453ea42321346d72d0cf955f5ef28c584076bed2
MinGyeom <87847136+reignkk1@users.noreply.github.com>
2024-03-18 15:35:11 +0900

    fix: Seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import Seo from "@/components/Seo";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Seo />
+      <Component {...pageProps} />
+    </>
+  );
 }
